75add4f2fab0b1207241cf08fc8d853e5373bffb
jianhan liu <lorgerd@163.com>
2017-08-02 00:18:03 +0800

    update

--- Machine_Learning_Coursera_Andrew-Ng/exercise/week3/test.m
+++ Machine_Learning_Coursera_Andrew-Ng/exercise/week3/test.m
@@ -1,4 +1,4 @@
-options = optimset('GradObj', 'on', 'MaxIter', '100');
+options = optimset('GradObj', 'on', 'MaxIter', 100);
 initialTheta = zeros(2,1);
 [optTheta, functionVal, exitFlag] = fminunc(@costFunction, initialTheta, options);
 
